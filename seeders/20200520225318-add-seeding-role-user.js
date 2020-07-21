d5bda9e5588dfd03310d9092ac0e4476d70401ae
reynalddff <35914709+reynalddff@users.noreply.github.com>
2020-07-21 15:27:34 +0700

    initial commit revisi bu faiz

--- seeders/20200520225318-add-seeding-role-user.js
+++ seeders/20200520225318-add-seeding-role-user.js
@@ -4,42 +4,46 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Roles', [
       {
+        id: 1,
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: 2,
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: 3,
+        role: 'operator umum',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: 4,
+        role: 'operator akademik',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: 5,
+        role: 'operator keuangan dan kepegawaian',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: 6,
+        role: 'operator kemahasiswaan',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: 7,
+        role: 'satpam',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
         id: 8,
         role: 'kasubbag',
         createdAt: new Date(),
         updatedAt: new Date()
       }
-      // }, {
-      //   id: 2,
-      //   role: 'admin',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }, {
-      //   id: 3,
-      //   role: 'operator umum',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }, {
-      //   id: 4,
-      //   role: 'operator akademik',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }, {
-      //   id: 5,
-      //   role: 'operator keuangan dan kepegawaian',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }, {
-      //   id: 6,
-      //   role: 'operator kemahasiswaan',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }, {
-      //   id: 7,
-      //   role: 'satpam',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }
     ], {});
   },
 
