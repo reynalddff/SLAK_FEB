af006483b4ec4c9c5d12a5a92ce6e280b906c915
reynalddff <35914709+reynalddff@users.noreply.github.com>
2020-12-24 16:21:34 +0700

    accept current changes on file migrations/create-detail-peminjaman.js

--- migrations/20201012054025-create-detail-peminjaman.js
+++ migrations/20201012054025-create-detail-peminjaman.js
@@ -1,37 +1,27 @@
-'use strict';
+"use strict";
 module.exports = {
-<<<<<<< HEAD:migrations/20201027181208-create-detail-peminjaman.js
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Detail_Peminjamans', {
-=======
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Detail_Peminjamans', {
->>>>>>> 545070e64a206e6c75356f6f5aaab26b2fe65221:migrations/20201012054025-create-detail-peminjaman.js
+    await queryInterface.createTable("Detail_Peminjamans", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       status: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-<<<<<<< HEAD:migrations/20201027181208-create-detail-peminjaman.js
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Detail_Peminjamans');
-=======
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Detail_Peminjamans');
->>>>>>> 545070e64a206e6c75356f6f5aaab26b2fe65221:migrations/20201012054025-create-detail-peminjaman.js
-  }
+    await queryInterface.dropTable("Detail_Peminjamans");
+  },
 };
